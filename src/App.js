53d85048c1f9c302a067d98f5339946e9c81f5d7
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-10 20:08:45 -0300

    feat: App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "react-auth-kit";
+import { useIsAuthenticated } from "react-auth-kit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { refreshApi } from "./services";
+import { User, Home, Login } from "./pages";
+
+const PrivateRoute = ({ Component, redirectTo, inverse = false }) => {
+  const isAuthenticated = useIsAuthenticated();
+  const auth = isAuthenticated();
+  if (inverse ? !auth : auth) {
+    return <Component />;
+  } else {
+    return <Navigate to={redirectTo} />;
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  render() {
+    return (
+      <AuthProvider
+        authType={"localstorage"}
+        authName={"_auth"}
+        refresh={refreshApi}
+      >
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<PrivateRoute Component={Home} redirectTo="/login" />}
+            />
+            <Route
+              exact
+              path="/users"
+              element={<PrivateRoute Component={User} redirectTo="/login" />}
+            />
+            <Route
+              exact
+              path="/login"
+              element={
+                <PrivateRoute Component={Login} redirectTo="/" inverse={true} />
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    );
+  }
 }
 
 export default App;
