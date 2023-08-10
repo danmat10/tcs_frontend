3c8417c1b447aeb7ea3a7ddd0a8107bbd632a1e8
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-10 20:02:08 -0300

    feat: Login/

---
+++ src/components/Login/LoginFormFields.component.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+const LoginFormFields = ({ formik }) => (
+  <>
+    <TextField
+      fullWidth
+      label="Login"
+      name="login"
+      type="text"
+      margin="dense"
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      value={formik.values.login}
+      error={formik.touched.login && Boolean(formik.errors.login)}
+      helperText={formik.touched.login && formik.errors.login}
+    />
+    <TextField
+      fullWidth
+      label="Senha"
+      name="password"
+      type="password"
+      margin="dense"
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      value={formik.values.password}
+      error={formik.touched.password && Boolean(formik.errors.password)}
+      helperText={formik.touched.password && formik.errors.password}
+    />
+  </>
+);
+
+export default LoginFormFields;
