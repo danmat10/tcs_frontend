72cc074fcb6256005ae44611e27623aa1f877dfe
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-11 21:57:17 -0300

    style: Sidebar

--- src/components/Header/Sidebar.component.js
+++ src/components/Header/Sidebar.component.js
@@ -6,9 +6,26 @@ import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import PeopleIcon from "@mui/icons-material/People";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+import { ReactComponent as Logo } from "../assets/icons/logo.svg";
 
 const Sidebar = ({ isOpen, onClose }) => (
-  <Drawer anchor="left" open={isOpen} onClose={onClose}>
+  <Drawer
+    anchor="left"
+    open={isOpen}
+    onClose={onClose}
+    PaperProps={{
+      sx: { width: "256px" },
+    }}
+  >
+    <Toolbar>
+      <Logo style={{ width: "76px" }} />
+      <Typography variant="h6">Sidebar Menu</Typography>
+    </Toolbar>
+    <Divider />
     <List>
       <ListItem component={Link} to="/users">
         <ListItemIcon>
