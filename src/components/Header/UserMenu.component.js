a56edb23a35ddbb5ef2f3e3dd45ec345beb3c29a
Daniel Matthes <danielmatthes10@gmail.com>
2023-08-14 13:35:56 -0300

    style: UserMenu

--- src/components/Header/UserMenu.component.js
+++ src/components/Header/UserMenu.component.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Typography from "@mui/material/Typography";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import ExitToApp from "@mui/icons-material/ExitToApp";
 
 const UserMenu = ({ anchorEl, onClose, onEditProfile, onLogout }) => (
   <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={onClose}>
-    <MenuItem onClick={onEditProfile}>Editar Perfil</MenuItem>
-    <MenuItem onClick={onLogout}>Logout</MenuItem>
+    <MenuItem onClick={onEditProfile}>
+      <ListItemIcon>
+        <AccountCircle fontSize="small" />
+      </ListItemIcon>
+      <Typography variant="inherit">Perfil</Typography>
+    </MenuItem>
+    <MenuItem onClick={onLogout}>
+      <ListItemIcon>
+        <ExitToApp fontSize="small" />
+      </ListItemIcon>
+      <Typography variant="inherit">Logout</Typography>
+    </MenuItem>
   </Menu>
 );
 
