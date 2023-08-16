3b256b62f1be6473ded51635ab92bf31ff42edf5
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-16 19:49:01 -0300

    refactor: Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,6 @@ import { LoginFormFields } from "../components/Login";
 import { validateLoginForm } from "../validations";
 import { ReactComponent as Logo } from "../components/assets/icons/logo.svg";
 
-
 const Login = (props) => {
   const signIn = useSignIn();
 
@@ -49,42 +48,48 @@ const Login = (props) => {
         tokenType: "Bearer",
         refreshToken: response.refresh_token,
         refreshTokenExpireIn: REFRESH_TOKEN_EXPIRES_AT,
-        authState: response.access_token,
+        authState: response.user,
       });
     }
   };
 
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
+    <Grid container component="main" sx={{ height: "100vh" }}>
       <Grid
         item
         xs={false}
         sm={4}
         md={7}
         sx={{
-          backgroundRepeat: 'no-repeat',
-          backgroundImage: 'linear-gradient(90.83deg, #0066FF 8.37%, #00C2FF 96.86%)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+          backgroundRepeat: "no-repeat",
+          backgroundImage:
+            "linear-gradient(90.83deg, #0066FF 8.37%, #00C2FF 96.86%)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
           alignItems: "center",
           justifyContent: "center",
-          display: { xs: 'none', sm: 'flex' }
+          display: { xs: "none", sm: "flex" },
         }}
       >
         <Logo style={{ width: "350px" }} />
       </Grid>
-      <Grid item xs={12} sm={8} md={5} elevation={6} sx={{ alignItems: "center", display: "flex", justifyContent: "center" }}>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        elevation={6}
+        sx={{ alignItems: "center", display: "flex", justifyContent: "center" }}
+      >
         <Box
           sx={{
             my: 8,
             mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          <Typography variant="h6">
-            Login
-          </Typography>
+          <Typography variant="h6">Login</Typography>
           <Typography variant="body2" sx={{ opacity: "0.6" }}>
             Realize o login no sistema
           </Typography>
@@ -98,7 +103,7 @@ const Login = (props) => {
             >
               Fazer Login
             </Button>
-            <Grid container >
+            <Grid container>
               <Grid item xs={6}>
                 <Link href="/forgot-password" variant="body2">
                   Primeiro Acesso?
