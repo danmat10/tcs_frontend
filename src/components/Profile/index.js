53089ed428b1e11c760ec95c5e3818f91702ad92
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-16 17:37:19 -0300

    style: Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,5 @@
+import styles from "./Profile.module.css";
+export { default as ProfileBreadcrumb } from "./ProfileBreadcrumb.component";
+export { default as ProfileTabAccount } from "./ProfileTabAccount.component";
+export { default as ProfileTabSecurity } from "./ProfileTabSecurity.component";
+export { styles };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,23 +1,53 @@
-import { Container, Grid, Typography } from "@mui/material"
+import { Container, Grid, Typography, Tabs, Tab } from "@mui/material";
+import React from "react";
 
-import { Header } from "../components/Header"
-import { UserBreadcrumb } from "../components/User"
-
+import { Header } from "../components/Header";
+import {
+  ProfileBreadcrumb,
+  ProfileTabAccount,
+  ProfileTabSecurity,
+} from "../components/Profile";
+import { styles } from "../components/Profile";
+import { AccountCircle, Lock } from "@mui/icons-material";
 
 const ProfilePage = () => {
+  const [selectedTab, setSelectedTab] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
   return (
     <>
       <Header />
       <Container maxWidth="xl">
         <Grid item lg={12} paddingY={5}>
-          <UserBreadcrumb />
+          <ProfileBreadcrumb />
           <Typography variant="h4" marginTop={2}>
-            Perfil
+            Editar Perfil
           </Typography>
         </Grid>
       </Container>
-      <Container maxWidth="xl"></Container>
-    </>)
-}
+      <Container maxWidth="xl">
+        <Grid container spacing={3} className={styles.listGrid}>
+          <Tabs
+            value={selectedTab}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab icon={<AccountCircle />} label="Conta" iconPosition="start" />
+            <Tab icon={<Lock />} label="Segurança" iconPosition="start" />
+          </Tabs>
+          <Grid item xs={12} md={12} lg={12}>
+            {selectedTab === 0 && <ProfileTabAccount />}
+            {selectedTab === 1 && <ProfileTabSecurity />}
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
