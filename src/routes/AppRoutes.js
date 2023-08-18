3ff614cbf48f95a028248f76d1c3279c2960cfd7
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-18 18:33:53 -0300

    feat: UserContext

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,22 +1,40 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+
+import UserContext from "../contexts/UserContext";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 import { ForgotPassword, Home, Login, Profile, User } from "../pages";
 
 const AppRoutes = () => {
+  const [user, setUser] = React.useState({ photo: null });
   return (
-    <Routes>
-      <Route exact path="/" element={<PrivateRoute Component={Home} />} />
-      <Route exact path="/users" element={<PrivateRoute Component={User} />} />
-      <Route exact path="/profile" element={<PrivateRoute Component={Profile} />} />
+    <UserContext.Provider value={{ user, setUser }}>
+      <Routes>
+        <Route exact path="/" element={<PrivateRoute Component={Home} />} />
+        <Route
+          exact
+          path="/users"
+          element={<PrivateRoute Component={User} />}
+        />
+        <Route
+          exact
+          path="/profile"
+          element={<PrivateRoute Component={Profile} />}
+        />
 
-      <Route exact path="/login" element={<PublicRoute Component={Login} />} />
-      <Route
-        exact
-        path="/forgot-password"
-        element={<PublicRoute Component={ForgotPassword} />}
-      />
-    </Routes>
+        <Route
+          exact
+          path="/login"
+          element={<PublicRoute Component={Login} />}
+        />
+        <Route
+          exact
+          path="/forgot-password"
+          element={<PublicRoute Component={ForgotPassword} />}
+        />
+      </Routes>
+    </UserContext.Provider>
   );
 };
 
