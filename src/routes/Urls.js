88704f45b321c961ccbb9ecb5c4c3cf47dc0c8dc
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-23 21:01:16 -0300

    feat: Urls

---
+++ src/routes/Urls.js
@@ -0,0 +1,13 @@
+const URLS = {
+  HOME: "/",
+  LOGIN: "/login",
+  USUARIO: "/user",
+  REQUISICAO: "/",
+  PATRIMONIO: "/",
+  DEPARTAMENTO: "/",
+  MANUTENCAO: "/",
+  GESTAO: "/",
+  PERFIL: "/profile",
+};
+
+export default URLS;
