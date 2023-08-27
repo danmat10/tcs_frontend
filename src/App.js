d65c3bebfde4b42000e72eca574d270f6830486f
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-27 16:02:42 -0300

    chore: installed typeface-roboto dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "react-auth-kit";
+import 'typeface-roboto';
 
 import "./App.css";
 import { refreshApi } from "services";
