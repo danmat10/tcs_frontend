7d9b1cf7e03340285c6888b53582dc8c5515bfb8
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-27 11:14:33 -0300

    feat: DepartmentFormFields

---
+++ src/components/Department/DepartmentFormFields.component.js
@@ -0,0 +1,52 @@
+import { TextField, Grid, FormHelperText, Autocomplete } from "@mui/material";
+import { styles } from ".";
+
+const DepartmentFormFields = ({ formik, userList = [] }) => {
+  return (
+    <div className={styles.formFields}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item md={11} xs={12}>
+          <TextField
+            fullWidth
+            label="Nome do Departamento"
+            name="nmDepartamento"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.nmDepartamento}
+            error={
+              formik.touched.nmDepartamento &&
+              Boolean(formik.errors.nmDepartamento)
+            }
+            helperText={
+              formik.touched.nmDepartamento && formik.errors.nmDepartamento
+            }
+          />
+        </Grid>
+        <Grid item md={11} xs={12}>
+          <Autocomplete
+            fullWidth
+            options={userList}
+            getOptionLabel={(option) => option.nmUsuario}
+            value={formik.values.usuario}
+            onChange={(event, newValue) => {
+              formik.setFieldValue("usuario", newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Responsável do Departamento"
+                error={formik.touched.usuario && Boolean(formik.errors.usuario)}
+                helperText={formik.touched.usuario && formik.errors.usuario}
+              />
+            )}
+          />
+        </Grid>
+      </Grid>
+      {formik.errors._errors && (
+        <FormHelperText error>{formik.errors._errors}</FormHelperText>
+      )}
+    </div>
+  );
+};
+
+export default DepartmentFormFields;
