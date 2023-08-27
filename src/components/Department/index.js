7df2c8d653b600d8b884be1a2588aee83ed89665
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-27 11:16:04 -0300

    refactor: DepartmentBreadcrumb

---
+++ src/components/Department/index.js
@@ -0,0 +1,8 @@
+import styles from "./Department.module.css";
+export { default as DepartmentBreadcrumb } from "./DepartmentBreadcrumb.component";
+export { default as DepartmentFormFields } from "./DepartmentFormFields.component";
+export { default as DepartmentCreate } from "./DepartmentCreate.component";
+export { default as DepartmentEdit } from "./DepartmentEdit.component";
+export { default as DepartmentList } from "./DepartmentList.component";
+export { default as DepartmentView } from "./DepartmentView.component";
+export { styles };
--- src/components/Departments/index.js
+++
@@ -1,1 +0,0 @@
-export { default as DepartmentsBreadcrumb } from "./DepartmentsBreadcrumb.component";
