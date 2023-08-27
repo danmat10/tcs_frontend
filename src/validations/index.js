54a435f9d2595050f4da45ddc01fa7960d509344
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-27 11:14:00 -0300

    feat: DepartmentValidations

--- src/validations/index.js
+++ src/validations/index.js
@@ -2,3 +2,5 @@ export { default as validateLoginForm } from "./LoginValidations";
 export { validateUserCreateForm } from "./UserValidations";
 export { validateUserEditForm } from "./UserValidations";
 export { validateContacts } from "./UserValidations";
+export { validateDepartmenCreateForm } from "./DepartmentValidations";
+export { validateDepartmenEditForm } from "./DepartmentValidations";
