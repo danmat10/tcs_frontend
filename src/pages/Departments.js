d6355407a66a11764765f1d2286a44ea1f045e30
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-27 11:16:33 -0300

    refactor: Department

--- src/pages/Departments.js
+++ src/pages/Departments.js
@@ -1,18 +1,142 @@
+import { useEffect, useState } from "react";
+import { useAuthHeader } from "react-auth-kit";
+import { Dialog } from "@mui/material";
+
 import { Breadcrumb, PageContainer } from "components/Common";
-import PageGridContent from "components/Common/PageGridContent.component";
-import { DepartmentsBreadcrumb } from "components/Departments";
 import { Header } from "components/Header";
+import { MESSAGES } from "config";
+import { ENDPOINTS, apiCall } from "services";
+import { DepartmentBreadcrumb, DepartmentCreate, DepartmentEdit, DepartmentList, DepartmentView } from "components/Department";
 
 const DepartmentsPage = () => {
+  useEffect(() => {
+    handleUpdateUserList();
+    handleUpdateDepartmentsList();
+  }, []);
+
+  const [state, setState] = useState({
+    view: "",
+    selectedDepartment: null,
+    openDialog: false,
+    users: [],
+    departments: [],
+  });
+  const authHeader = useAuthHeader();
+
+  const openDialog = (view, department = null) => {
+    setState((prev) => ({
+      ...prev,
+      view,
+      openDialog: true,
+      selectedDepartment: department,
+    }));
+  };
+
+  const closeDialog = () => {
+    setState((prev) => ({ ...prev, view: "", openDialog: false }));
+  };
+
+  const handleApiCall = async (config, toastObject) => {
+    const { method, endpoint, data } = config;
+    const response = await apiCall(
+      method,
+      endpoint,
+      data,
+      {
+        Authorization: authHeader(),
+      },
+      toastObject
+    );
+    return response;
+  };
+
+  const handleCreateDepartment = async (department) => {
+    await handleApiCall(
+      { method: "post", endpoint: ENDPOINTS.DEPARTMENT.POST, data: department },
+      MESSAGES.DEPARTMENT.POST
+    );
+    handleUpdateDepartmentsList();
+  };
+
+  const handleUpdateUserList = async () => {
+    let users = await handleApiCall(
+      {
+        method: "get",
+        endpoint: ENDPOINTS.USER.GET,
+      },
+      MESSAGES.USER.GET
+    );
+    if (!users) users = [];
+    setState((prev) => ({ ...prev, users }));
+  };
+
+  const handleUpdateDepartmentsList = async () => {
+    let departments = await handleApiCall(
+      {
+        method: "get",
+        endpoint: ENDPOINTS.DEPARTMENT.GET,
+      },
+      MESSAGES.DEPARTMENT.GET
+    );
+    if (!departments) departments = [];
+    setState((prev) => ({ ...prev, departments }));
+  };
+
+  const handleEditDepartment = async (department) => {
+    await handleApiCall(
+      {
+        method: "patch",
+        endpoint: ENDPOINTS.DEPARTMENT.PATCH_id(department.id),
+        data: department,
+      },
+      MESSAGES.DEPARTMENT.PATCH
+    );
+    handleUpdateDepartmentsList();
+  };
+
+  const views = {
+    list: (
+      <DepartmentList departments={state.departments} openDialog={openDialog} />
+    ),
+    create: (
+      <DepartmentCreate
+        onCreate={handleCreateDepartment}
+        onClose={closeDialog}
+        users={state.users}
+      />
+    ),
+    update: (
+      <DepartmentEdit
+        department={state.selectedDepartment}
+        users={state.users}
+        onUpdate={handleEditDepartment}
+        onClose={closeDialog}
+      />
+    ),
+    view: (
+      <DepartmentView
+        department={state.selectedDepartment}
+        onClose={closeDialog}
+      />
+    ),
+  };
+
   return (
     <>
       <Header />
       <PageContainer>
         <Breadcrumb title="Departamentos">
-          <DepartmentsBreadcrumb />
+          <DepartmentBreadcrumb />
         </Breadcrumb>
-        <PageGridContent></PageGridContent>
+        {views.list}
       </PageContainer>
+      <Dialog
+        open={state.openDialog}
+        onClose={closeDialog}
+        PaperProps={{ sx: { borderRadius: "28px" } }}
+      >
+        {views[state.view]}
+      </Dialog>
     </>
   );
 };
