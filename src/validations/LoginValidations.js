97b37bedd18ff658dea552dc5cf7b758b0cd4307
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-29 17:46:57 -0300

    refactor: \validations

--- src/validations/LoginValidations.js
+++ src/validations/LoginValidations.js
@@ -1,11 +1,19 @@
 const validateLoginForm = (values) => {
   const errors = {};
-  if (!values.login) {
+
+  const trimmedLogin = values.login?.trim();
+  if (!trimmedLogin) {
     errors.login = "Obrigatório";
+  } else if (trimmedLogin.length < 3) {
+    errors.login = "O login digitado é muito curto";
   }
+
   if (!values.password) {
     errors.password = "Obrigatório";
+  } else if (values.password.length < 4) {
+    errors.password = "A senha digitada é muito curta";
   }
+
   return errors;
 };
 
--- src/validations/UserValidations.js
+++ src/validations/UserValidations.js
@@ -30,6 +30,22 @@ function validateContacts(contacts, user = {}) {
       if (!contato.dsContato) {
         contatoErrors.dsContato = "Obrigatório";
       }
+
+      if (
+        contato.typeContacts === "E-mail" &&
+        !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(contato.dsContato)
+      ) {
+        contatoErrors.dsContato = "E-mail inválido";
+      }
+
+      if (contato.typeContacts === "Telefone" && contato.dsContato.length < 8) {
+        contatoErrors.dsContato = "Telefone inválido. Digite um número válido";
+      }
+
+      if (contato.typeContacts === "Celular" && contato.dsContato.length < 9) {
+        contatoErrors.dsContato = "Celular inválido. Digite um número válido";
+      }
+
       return contatoErrors;
     });
 
@@ -66,12 +82,22 @@ function validateContacts(contacts, user = {}) {
 const validateUserCreateForm = (values) => {
   const errors = validateContacts(values.contacts);
 
-  if (!values.nmUsuario) {
+  const trimmedName = values.nmUsuario?.trim();
+  if (!trimmedName) {
     errors.nmUsuario = "Obrigatório";
+  } else if (trimmedName.length < 3) {
+    errors.nmUsuario = "O nome do usuário deve ter pelo menos 3 caracteres";
   }
-  if (!values.nrMatricula) {
+
+  const trimmedMatricula = values.nrMatricula?.trim();
+  if (!trimmedMatricula) {
     errors.nrMatricula = "Obrigatório";
+  } else if (trimmedMatricula.length < 3) {
+    errors.nrMatricula = "A matrícula deve ter pelo menos 3 caracteres";
+  } else if (!/^\d+$/.test(trimmedMatricula)) {
+    errors.nrMatricula = "A matrícula deve conter apenas números";
   }
+
   if (!values.typeUser) {
     errors.typeUser = "Obrigatório";
   }
@@ -86,6 +112,7 @@ const validateUserCreateForm = (values) => {
 
 const validateUserEditForm = (values, user) => {
   const errors = validateUserCreateForm(values);
+
   const contactsErrors = validateContacts(values.contacts);
   if (contactsErrors.contacts) {
     errors.contacts = contactsErrors.contacts;
