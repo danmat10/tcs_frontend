0910c9da29e312b5a7e968e6dad43ae61c7eaada
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-31 17:46:44 -0300

    feat: ConstructionCreate

---
+++ src/components/Construction/ConstructionCreate.component.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useAuthHeader } from "react-auth-kit";
+import { FormikProvider, useFormik } from "formik";
+
+import { DialogForm } from "components/Common";
+import ConstructionFormFields from "./ConstructionFormFields.component";
+import { validateConstructionCreateForm } from "validations";
+
+const ConstructionCreate = ({ onClose, users, setState }) => {
+  const authHeader = useAuthHeader();
+
+  const formik = useFormik({
+    initialValues: {
+      nmObra: "",
+      usuario: null,
+      nmCpf: "",
+      nmCliente: "",
+      endereco: {
+        nmCep: "",
+        nmBairro: "",
+        nmLogradouro: "",
+        nmNumero: "",
+        nmComplemento: "",
+        nmCidade: "",
+        nmEstado: "",
+      },
+      dtInicio: "",
+      dtPrevisaoFinalizacao: "",
+      dtFinalizacao: "",
+    },
+    validateOnChange: false,
+    validate: (values) => validateConstructionCreateForm(values),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const filteredUserList = users.filter((usuario) => {
+    if (usuario.flStatus === "Inativo") {
+      return false;
+    }
+    return true;
+  });
+
+  return (
+    <DialogForm
+      title="Cadastrar Obra"
+      onClose={onClose}
+      onSubmit={() => formik.submitForm()}
+      btnSubmitName="Cadastrar"
+    >
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <ConstructionFormFields formik={formik} userList={filteredUserList} />
+        </form>
+      </FormikProvider>
+    </DialogForm>
+  );
+};
+
+export default ConstructionCreate;
