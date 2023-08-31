871a70ab3d75974a08d47ff6870ca76f1575626a
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-31 17:44:54 -0300

    feat: UfField

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -3,4 +3,5 @@ export { default as Breadcrumb } from "./Breadcrumb.component";
 export { default as DialogForm } from "./DialogForm.component";
 export { default as PageContainer } from "./PageContainer.component";
 export { default as CpfCnpjMask } from "./CpfCnpjMask.component";
+export { default as UfField } from "./UfField.component";
 export { styles };
