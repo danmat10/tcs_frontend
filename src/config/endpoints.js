72a732e47fbc5308f0e9901d8d8f369cf066514f
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-31 22:57:47 -0300

    fix: endpoints

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -13,7 +13,6 @@ const ENDPOINTS = {
     GET_ID: (id) => BASEURL + "/api/users/" + id,
     PATCH: BASEURL + "/api/users/",
     PATCH_ID: (id) => BASEURL + "/api/users/" + id,
-    DELETE: BASEURL + "/api/users/",
     PROFILE: {
       GET_PHOTO: (id) => BASEURL + "/uploads/" + id,
       POST_PHOTO: (id) => BASEURL + "/users/" + id + "/photo",
@@ -25,13 +24,11 @@ const ENDPOINTS = {
     GET: BASEURL + "/api/departments",
     POST: BASEURL + "/api/departments",
     PATCH_id: (id) => BASEURL + "/api/departments/" + id,
-    DELETE: BASEURL + "/api/departments/",
   },
   CONSTRUCTION: {
-    GET: BASEURL + "/db/constructions",
-    POST: BASEURL + "/db/constructions",
-    PATCH_id: (id) => BASEURL + "/db/constructions/" + id,
-    DELETE: BASEURL + "/db/constructions/",
+    GET: BASEURL + "/api/constructions",
+    POST: BASEURL + "/api/constructions",
+    PATCH_id: (id) => BASEURL + "/api/constructions/" + id,
   },
 };
 
