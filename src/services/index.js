9f423fd3f632166bab206661bf1584a378d50316
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-31 22:44:39 -0300

    refactor: update services exports

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,6 @@
-export {
-  handleCreateDepartment,
-  handleGetDepartmentsList,
-  handleEditDepartment,
-} from "./departmentCalls";
-export {
-  handleCreateUser,
-  handleEditUser,
-  handleGetUsersList,
-} from "./userCalls";
-export { handleLogin, handleEditPassword } from "./authCalls";
-export { handleEditContacts, handleUploadPhoto } from "./profileCalls";
-export { apiCall, handleApiCall, refreshApi, handleBuscaCep } from "./apiUtils";
+export * from "./departmentCalls";
+export * from "./userCalls";
+export * from "./authCalls";
+export * from "./profileCalls";
+export * from "./apiUtils";
 export * from "./constructionCalls";
