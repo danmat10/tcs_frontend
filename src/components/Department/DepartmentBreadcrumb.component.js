12f17979e90e6837a61f0548312b28f053be194f
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-09-02 18:33:22 -0300

    refactor: update DepartmentBreadcrumb imports

--- src/components/Department/DepartmentBreadcrumb.component.js
+++ src/components/Department/DepartmentBreadcrumb.component.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Typography } from "@mui/material";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
+import { Breadcrumbs, Typography, Link } from "@mui/material";
 import { BusinessCenter, Home } from "@mui/icons-material";
+import { URLS } from "config";
 
 const DepartmentBreadcrumb = () => {
   return (
     <Breadcrumbs aria-label="breadcrumb">
       <Link
         color="inherit"
-        href="/"
+        href={URLS.HOME}
         style={{ display: "flex", alignItems: "center" }}
       >
         <Home style={{ marginRight: "5px" }} />
