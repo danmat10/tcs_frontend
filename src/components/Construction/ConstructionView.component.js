6df4e8c2f3a75298bd3f6bcabc9200968e58e381
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-09-02 15:34:03 -0300

    refactor: add DialogForm in component view

--- src/components/Construction/ConstructionView.component.js
+++ src/components/Construction/ConstructionView.component.js
@@ -10,196 +10,185 @@ import {
   Typography,
 } from "@mui/material";
 
-import { styles } from ".";
+import { DialogForm } from "components/Common";
 
 const ConstructionView = ({ construction, onClose }) => {
   return (
-    <>
-      <DialogTitle className={styles.dialogTitle} paragraph>
-        Visualizar Obra
-      </DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2} alignItems="center" marginTop="auto">
-          <Grid item md={12} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Nome da Obra"
-              name="nmObra"
-              type="text"
-              value={construction.nmObra || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Responsável da Obra"
-              name="nmResponsavel"
-              type="text"
-              value={construction?.usuario?.nmUsuario || ""}
-              variant="standard"
-            />
-          </Grid>
+    <DialogForm title="Visualizar Obra" onClose={onClose}>
+      <Grid container spacing={2} alignItems="center" marginTop="auto">
+        <Grid item md={12} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Nome da Obra"
+            name="nmObra"
+            type="text"
+            value={construction.nmObra || ""}
+            variant="standard"
+          />
         </Grid>
-        <Grid container spacing={2} alignItems="center" marginTop="auto">
-          <Grid item md={12} xs={12}>
-            <Typography variant="subtitle1">Cliente</Typography>
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="CPF/CNPJ"
-              name="nmCpf"
-              type="text"
-              value={construction?.nmCpf || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Nome do Cliente"
-              name="nmCliente"
-              type="text"
-              value={construction?.nmCliente || ""}
-              variant="standard"
-            />
-          </Grid>
+        <Grid item md={12} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Responsável da Obra"
+            name="nmResponsavel"
+            type="text"
+            value={construction?.usuario?.nmUsuario || ""}
+            variant="standard"
+          />
         </Grid>
-        <Grid container spacing={2} alignItems="center" marginTop="auto">
-          <Grid item md={12} xs={12}>
-            <Typography variant="subtitle1">Endereço da Obra</Typography>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="CEP"
-              name="endereco.nmCep"
-              type="text"
-              value={construction?.endereco?.nmCep || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Bairro"
-              name="endereco.nmBairro"
-              type="text"
-              value={construction?.endereco?.nmBairro || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Logradouro"
-              name="endereco.nmLogradouro"
-              type="text"
-              value={construction?.endereco?.nmLogradouro || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={4} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Número"
-              name="endereco.nmNumero"
-              type="text"
-              value={construction?.endereco?.nmNumero || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={8} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Complemento"
-              name="endereco.nmComplemento"
-              type="text"
-              value={construction?.endereco?.nmComplemento || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Cidade"
-              name="endereco.nmCidade"
-              type="text"
-              value={construction?.endereco?.nmCidade || ""}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Estado"
-              name="endereco.nmEstado"
-              type="text"
-              value={construction?.endereco?.nmEstado || ""}
-              variant="standard"
-            />
-          </Grid>
+      </Grid>
+      <Grid container spacing={2} alignItems="center" marginTop="auto">
+        <Grid item md={12} xs={12}>
+          <Typography variant="subtitle1">Cliente</Typography>
         </Grid>
-        <Grid container spacing={2} alignItems="center" marginTop="auto">
-          <Grid item md={12} xs={12}>
-            <Typography variant="subtitle1">Datas</Typography>
-          </Grid>
+        <Grid item md={12} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="CPF/CNPJ"
+            name="nmCpf"
+            type="text"
+            value={construction?.nmCpf || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={12} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Nome do Cliente"
+            name="nmCliente"
+            type="text"
+            value={construction?.nmCliente || ""}
+            variant="standard"
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} alignItems="center" marginTop="auto">
+        <Grid item md={12} xs={12}>
+          <Typography variant="subtitle1">Endereço da Obra</Typography>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="CEP"
+            name="endereco.nmCep"
+            type="text"
+            value={construction?.endereco?.nmCep || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Bairro"
+            name="endereco.nmBairro"
+            type="text"
+            value={construction?.endereco?.nmBairro || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={12} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Logradouro"
+            name="endereco.nmLogradouro"
+            type="text"
+            value={construction?.endereco?.nmLogradouro || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Número"
+            name="endereco.nmNumero"
+            type="text"
+            value={construction?.endereco?.nmNumero || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={8} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Complemento"
+            name="endereco.nmComplemento"
+            type="text"
+            value={construction?.endereco?.nmComplemento || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Cidade"
+            name="endereco.nmCidade"
+            type="text"
+            value={construction?.endereco?.nmCidade || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Estado"
+            name="endereco.nmEstado"
+            type="text"
+            value={construction?.endereco?.nmEstado || ""}
+            variant="standard"
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} alignItems="center" marginTop="auto">
+        <Grid item md={12} xs={12}>
+          <Typography variant="subtitle1">Datas</Typography>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Data de Início"
+            name="dtInicio"
+            type="date"
+            value={construction?.dtInicio || ""}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <TextField
+            disabled
+            fullWidth
+            label="Previsão de Finalização"
+            name="dtPrevisaoFinalizacao"
+            type="date"
+            value={construction?.dtPrevisaoFinalizacao || ""}
+            variant="standard"
+          />
+        </Grid>
+        {construction?.dtFinalizacao && (
           <Grid item md={6} xs={12}>
             <TextField
               disabled
               fullWidth
-              label="Data de Início"
-              name="dtInicio"
+              label="Data de Finalização"
+              name="dtFinalizacao"
               type="date"
-              value={construction?.dtInicio || ""}
+              value={construction?.dtFinalizacao || ""}
               variant="standard"
             />
           </Grid>
-          <Grid item md={6} xs={12}>
-            <TextField
-              disabled
-              fullWidth
-              label="Previsão de Finalização"
-              name="dtPrevisaoFinalizacao"
-              type="date"
-              value={construction?.dtPrevisaoFinalizacao || ""}
-              variant="standard"
-            />
-          </Grid>
-          {construction?.dtFinalizacao && (
-            <Grid item md={6} xs={12}>
-              <TextField
-                disabled
-                fullWidth
-                label="Data de Finalização"
-                name="dtFinalizacao"
-                type="date"
-                value={construction?.dtFinalizacao || ""}
-                variant="standard"
-              />
-            </Grid>
-          )}
-        </Grid>
-        <Divider sx={{ marginTop: 5 }} />
-        <DialogActions>
-          <Button variant="contained" color="primary" onClick={() => onClose()}>
-            OK
-          </Button>
-        </DialogActions>
-      </DialogContent>
-    </>
+        )}
+      </Grid>
+    </DialogForm>
   );
 };
 
--- src/components/User/UserView.component.js
+++ src/components/User/UserView.component.js
@@ -1,89 +1,69 @@
 import React from "react";
-import {
-  DialogContent,
-  DialogTitle,
-  Chip,
-  TextField,
-  Grid,
-  Divider,
-  DialogActions,
-  Button,
-} from "@mui/material";
+import { Chip, TextField, Grid } from "@mui/material";
 
-import { styles } from ".";
+import { DialogForm } from "components/Common";
 
 const UserView = ({ user, onClose }) => {
   return (
-    <>
-      <DialogTitle className={styles.userDialogTitle} paragraph>
-        Visualizar Usuário
-      </DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={12} align="right">
-            {user.flStatus === "Ativo" ? (
-              <Chip label="Ativo" color="success" />
-            ) : (
-              <Chip label="Inativo" color="error" />
-            )}
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              label="Nome"
-              value={user.nmUsuario}
-              variant="standard"
-              disabled
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              label="Matricula"
-              value={user.nrMatricula}
-              variant="standard"
-              disabled
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              label="CPF"
-              value={user.nrCpf}
-              variant="standard"
-              disabled
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              label="Tipo de Usuário"
-              value={user.typeUser}
-              variant="standard"
-              disabled
-              fullWidth
-            />
-          </Grid>
-          {user.contacts &&
-            user.contacts.map((contact, index) => (
-              <Grid item xs={12} md={6} key={index}>
-                <TextField
-                  label={contact.typeContacts}
-                  value={contact.dsContato}
-                  variant="standard"
-                  disabled
-                  fullWidth
-                />
-              </Grid>
-            ))}
+    <DialogForm title="Visualizar Usuário" onClose={onClose}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12} align="right">
+          {user.flStatus === "Ativo" ? (
+            <Chip label="Ativo" color="success" />
+          ) : (
+            <Chip label="Inativo" color="error" />
+          )}
         </Grid>
-        <Divider sx={{ marginTop: 5 }} />
-        <DialogActions>
-          <Button variant="contained" color="primary" onClick={() => onClose()}>
-            OK
-          </Button>
-        </DialogActions>
-      </DialogContent>
-    </>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Nome"
+            value={user.nmUsuario}
+            variant="standard"
+            disabled
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Matricula"
+            value={user.nrMatricula}
+            variant="standard"
+            disabled
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="CPF"
+            value={user.nrCpf}
+            variant="standard"
+            disabled
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            label="Tipo de Usuário"
+            value={user.typeUser}
+            variant="standard"
+            disabled
+            fullWidth
+          />
+        </Grid>
+        {user.contacts &&
+          user.contacts.map((contact, index) => (
+            <Grid item xs={12} md={6} key={index}>
+              <TextField
+                label={contact.typeContacts}
+                value={contact.dsContato}
+                variant="standard"
+                disabled
+                fullWidth
+              />
+            </Grid>
+          ))}
+      </Grid>
+    </DialogForm>
   );
 };
 
