1507832cf5e25f6d65889fd834a9168b10ac597b
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-09-02 15:09:20 -0300

    feat: Add handleEditConstruction

--- src/services/constructionCalls.js
+++ src/services/constructionCalls.js
@@ -1,31 +1,47 @@
-const { ENDPOINTS } = require("config");
-const { handleApiCall } = require("./apiUtils");
-const { MESSAGES } = require("config");
+import { handleApiCall } from ".";
+import { ENDPOINTS, MESSAGES } from "config";
 
 const handleCreateConstruction = async ({ data, header, setState }) => {
-    await handleApiCall(
-        {
-            method: "post",
-            endpoint: ENDPOINTS.CONSTRUCTION.POST,
-            data: data,
-            header: header,
-        },
-        MESSAGES.CONSTRUCTION.POST
-    );
-    handleGetConstructionList({ header, setState });
+  await handleApiCall(
+    {
+      method: "post",
+      endpoint: ENDPOINTS.CONSTRUCTION.POST,
+      data: data,
+      header: header,
+    },
+    MESSAGES.CONSTRUCTION.POST
+  );
+  handleGetConstructionList({ header, setState });
 };
 
 const handleGetConstructionList = async ({ header, setState }) => {
-    let constructions = await handleApiCall(
-        {
-            method: "get",
-            endpoint: ENDPOINTS.CONSTRUCTION.GET,
-            header: header,
-        },
-        MESSAGES.CONSTRUCTION.GET
-    );
-    if (!constructions) constructions = [];
-    setState((prev) => ({ ...prev, constructions }));
+  let constructions = await handleApiCall(
+    {
+      method: "get",
+      endpoint: ENDPOINTS.CONSTRUCTION.GET,
+      header: header,
+    },
+    MESSAGES.CONSTRUCTION.GET
+  );
+  if (!constructions) constructions = [];
+  setState((prev) => ({ ...prev, constructions }));
 };
 
-export { handleCreateConstruction, handleGetConstructionList }
+const handleEditConstruction = async ({ data, header, setState }) => {
+  await handleApiCall(
+    {
+      method: "put",
+      endpoint: ENDPOINTS.CONSTRUCTION.PUT_ID(data.id),
+      data: data,
+      header: header,
+    },
+    MESSAGES.CONSTRUCTION.PUT
+  );
+  handleGetConstructionList({ header, setState });
+};
+
+export {
+  handleCreateConstruction,
+  handleGetConstructionList,
+  handleEditConstruction,
+};
