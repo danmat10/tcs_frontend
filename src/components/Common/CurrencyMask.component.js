10e69c68d02b301b8db035b1da5151af50586d87
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-09-03 15:37:12 -0300

    refactor: CurrencyMask

--- src/components/Common/CurrencyMask.component.js
+++ src/components/Common/CurrencyMask.component.js
@@ -1,26 +1,26 @@
 import { TextField } from "@mui/material";
 
+const maskCurrencyFunction = (value) => {
+  let cleanValue = value.replace(/\D/g, "");
+  let len = cleanValue.length;
+
+  if (cleanValue === "0" || len === 0) {
+    return "R$ 0,00";
+  }
+
+  if (len <= 2) {
+    return `R$ 0,${cleanValue}`;
+  }
+
+  cleanValue = `${cleanValue.slice(0, -2)}.${cleanValue.slice(-2)}`;
+  const parts = cleanValue.split(".");
+  const integerPart = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  const decimalPart = parts[1];
+
+  return `R$ ${integerPart},${decimalPart}`;
+};
+
 const CurrencyMask = ({ formik, fieldName, label }) => {
-  const maskCurrency = (value) => {
-    let cleanValue = value.replace(/\D/g, "");
-    let len = cleanValue.length;
-
-    if (cleanValue === "0" || len === 0) {
-      return "R$ 0,00";
-    }
-
-    if (len <= 2) {
-      return `R$ 0,${cleanValue}`;
-    }
-
-    cleanValue = `${cleanValue.slice(0, -2)}.${cleanValue.slice(-2)}`;
-    const parts = cleanValue.split(".");
-    const integerPart = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    const decimalPart = parts[1];
-
-    return `R$ ${integerPart},${decimalPart}`;
-  };
-
   return (
     <TextField
       fullWidth
@@ -31,11 +31,11 @@ const CurrencyMask = ({ formik, fieldName, label }) => {
         const rawValue = e.target.value.replace(/\D/g, "");
         formik.setFieldValue(fieldName, parseFloat(rawValue) / 100);
       }}
-      value={maskCurrency(String(formik.values[fieldName] * 100))}
+      value={maskCurrencyFunction(String(formik.values[fieldName] * 100))}
       error={formik.touched[fieldName] && Boolean(formik.errors[fieldName])}
       helperText={formik.touched[fieldName] && formik.errors[fieldName]}
     />
   );
 };
 
-export { CurrencyMask };
+export { CurrencyMask, maskCurrencyFunction };
