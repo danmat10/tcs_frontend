0a30a861487ea077ab1ae6700b5526d2c17b8262
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-09-03 15:38:09 -0300

    feat: Add PatrimonyList columns

--- src/components/Patrimony/PatrimonyList.component.js
+++ src/components/Patrimony/PatrimonyList.component.js
@@ -10,11 +10,84 @@ import { DataGrid, ptBR } from "@mui/x-data-grid";
 
 import { styles } from ".";
 import { PageGridContent } from "components/Common";
-import { Search } from "@mui/icons-material";
+import { Edit, Visibility } from "@mui/icons-material";
 
 const PatrimonyList = ({ patrimonies, openDialog }) => {
   const isMobile = useMediaQuery("(max-width:600px)");
   const [search, setSearch] = useState("");
+  const columns = getColumns(isMobile);
+
+  function getColumns(isMobile) {
+    const baseColumns = [
+      {
+        field: "nmSerie",
+        headerName: "Número de Série",
+        flex: 2,
+      },
+      {
+        field: "nmPatrimonio",
+        headerName: "Nome",
+        flex: 2,
+      },
+      {
+        field: "nmFornecedor",
+        headerName: "Fornecedor",
+        flex: 2,
+      },
+      {
+        field: "actions",
+        headerName: "Ações",
+        flex: 1,
+        align: "center",
+        headerAlign: "center",
+        renderCell: (params) => (
+          <>
+            <Visibility
+              color="primary"
+              onClick={() => {
+                openDialog("view", params.row);
+              }}
+              style={{ cursor: "pointer" }}
+              titleAccess="Visualizar"
+            />
+            <Edit
+              color="primary"
+              onClick={() => openDialog("update", params.row)}
+              style={{ cursor: "pointer" }}
+              titleAccess="Editar"
+            />
+          </>
+        ),
+      },
+    ];
+    if (isMobile) {
+      return baseColumns
+        .filter(
+          (column) =>
+            column.field === "nmPatrimonio" || column.field === "actions"
+        )
+        .map((column) => {
+          if (column.field === "nmPatrimonio") {
+            return { ...column, flex: 1 };
+          } else if (column.field === "actions") {
+            return { ...column, flex: 1 };
+          }
+          return column;
+        });
+    }
+    return baseColumns;
+  }
+
+  function matchesSearch(row) {
+    return [row.nmPatrimonio, row.nmFornecedor].some(
+      (value) =>
+        typeof value === "string" &&
+        value.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+  const filteredRows = patrimonies.filter((patrimony) => {
+    return matchesSearch(patrimony);
+  });
 
   return (
     <PageGridContent>
@@ -26,15 +99,9 @@ const PatrimonyList = ({ patrimonies, openDialog }) => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <FormHelperText>Pesquisar por nome, responsável...</FormHelperText>
+        <FormHelperText>Pesquisar por nome, fornecedor...</FormHelperText>
       </Grid>
-      <Grid item xs={12} md={3}>
-        <Button variant="contained" onClick={() => null}>
-          <Search />
-        </Button>
-      </Grid>
-
-      <Grid item xs={12} md={6} className={styles.buttonGrid}>
+      <Grid item xs={12} md={9} className={styles.buttonGrid}>
         <Button
           variant="contained"
           onClick={() => openDialog("create")}
@@ -44,7 +111,18 @@ const PatrimonyList = ({ patrimonies, openDialog }) => {
         </Button>
       </Grid>
       <Grid item xs={12} md={12} lg={12}>
-        <Grid item xs={12} md={12} lg={12}></Grid>
+        <Grid item xs={12} md={12} lg={12}>
+          <DataGrid
+            sx={{ height: 500 }}
+            rows={filteredRows}
+            columns={columns}
+            autoPageSize
+            pageSizeOptions={[10]}
+            checkboxSelection={false}
+            disableSelectionOnClick
+            localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+          />
+        </Grid>
       </Grid>
     </PageGridContent>
   );
